1f7689910d1303513da8f19caddbcd20750c1df9
Adem Kedir <ademkedir724@gmail.com>
2024-08-11 11:24:56 +0300

    connect db

--- server/server.js
+++ server/server.js
@@ -1,3 +1,16 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
-app.listen(3090, () => console.log('app is listening on 3090'));
+dotenv.config({ path: './.env' });
+
+const DB_URL = process.env.DATABASE_URL.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB_URL).then((conn) => {
+  console.log('db is connected');
+});
+
+const server = app.listen(3090, () => console.log('app is listening on 3090'));
