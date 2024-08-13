4a5e70e1f05044668c417ad52798287971360bd9
Adem Kedir <ademkedir724@gmail.com>
2024-08-13 10:11:59 +0300

    getUsers  from database

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,37 @@
 const UserModel = require('./../models/UserModel');
 
-const getAllUsers = (req, res, next) => {
-  console.log('getAll user');
-  return res.status(200).json({ status: 'success' });
+const getAllUsers = async (req, res, next) => {
+  try {
+    console.log('getAll user');
+    console.log(req.query);
+    const query = UserModel.find().sort('role name').select('-__v -updatedAt');
+    const users = await query;
+    return res.status(200).json({ status: 'success', data: { users } });
+  } catch (err) {
+    return res.status(404).json({
+      status: 'failure',
+      error: 'no Users data',
+    });
+  }
 };
 
-const getUserById = (req, res, next) => {
-  console.log('create user by id', req.params.id);
-  return res.status(200).json({ status: 'success' });
+const getUserById = async (req, res, next) => {
+  try {
+    console.log('get user by id', req.params.id);
+    const query = UserModel.findById(req.params.id).select('-__v -updatedAt');
+    const user = await query;
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    return res.status(404).json({
+      status: 'failure',
+      error: 'no user with given id',
+    });
+  }
 };
 const createUser = async (req, res, next) => {
   try {
