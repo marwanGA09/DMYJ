c9480d2422a4695849af734aed8fa77c126b0ee7
Adem Kedir <ademkedir724@gmail.com>
2024-08-13 12:33:57 +0300

    change mongoose validation and cast error into custom Operational error for production error handling

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require('../utils/AppError');
+
 const productionError = (err, res) => {
   console.log('from production');
   if (!err.isOperational) {
@@ -23,13 +25,33 @@ const developmentError = (err, res) => {
   });
 };
 
-const errorController = (err, req, res, next) => {
-  // console.log(err);
+// THIS BELLOW TWO (handleCastError, handleValidationError) FUNCTION IS USED TO MAKE MONGOOSE ERROR INTO OPERATIONAL
+const handleCastError = (err) => {
+  // Transfer CastError into operational error
+  // 400 bad request
+  return new AppError(`Invalid ${err.path}: ${err.value}`, 400);
+};
 
-  // err.message = err.message || 'some thing goes very wrong';
+const handleValidationError = (err) => {
+  // Transfer validationError into operational error
+  return new AppError(err.message, 400);
+};
+
+const errorController = (err, req, res, next) => {
+  console.log(err.message);
+
+  err.message = err.message || 'some thing goes very wrong';
+  // status and statusCode can be added for mongoose error
   err.status = err.status || 'error';
   err.statusCode = err.statusCode || 500;
   if (process.env.NODE_ENV === 'production') {
+    // let error = { ...err };
+    if (err.name === 'CastError') {
+      err = handleCastError(err);
+    }
+    if (err.name === 'ValidationError') {
+      err = handleValidationError(err);
+    }
     productionError(err, res);
   } else if (process.env.NODE_ENV === 'development') {
     developmentError(err, res);
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,61 +1,44 @@
+const catchAsync = require('../utils/catchAsync');
 const UserModel = require('./../models/UserModel');
 
-const getAllUsers = async (req, res, next) => {
-  try {
-    console.log('getAll user');
-    console.log(req.query);
-    const query = UserModel.find().sort('role name').select('-__v -updatedAt');
-    const users = await query;
-    return res.status(200).json({ status: 'success', data: { users } });
-  } catch (err) {
-    return res.status(404).json({
-      status: 'failure',
-      error: 'no Users data',
-    });
-  }
-};
+const getAllUsers = catchAsync(async (req, res, next) => {
+  console.log('getAll user');
+  console.log(req.query);
+  const query = UserModel.find().sort('role name').select('-__v -updatedAt');
+  const users = await query;
+  return res.status(200).json({ status: 'success', data: { users } });
+});
 
-const getUserById = async (req, res, next) => {
-  try {
-    console.log('get user by id', req.params.id);
-    const query = UserModel.findById(req.params.id).select('-__v -updatedAt');
-    const user = await query;
-    return res.status(200).json({
-      status: 'success',
-      data: {
-        user,
-      },
-    });
-  } catch (err) {
-    return res.status(404).json({
-      status: 'failure',
-      error: 'no user with given id',
-    });
-  }
-};
-const createUser = async (req, res, next) => {
-  try {
-    const { name, userName, sector, role, password } = req.body;
+const getUserById = catchAsync(async (req, res, next) => {
+  console.log('get user by id', req.params.id);
+  const query = UserModel.findById(req.params.id).select('-__v -updatedAt');
+  const user = await query;
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
+const createUser = catchAsync(async (req, res, next) => {
+  const { name, userName, sector, role, password } = req.body;
 
-    console.log(name, userName, sector, role, password);
+  console.log(name, userName, sector, role, password);
 
-    const user = await new UserModel({
-      name,
-      userName,
-      sector,
-      role,
-      password,
-    }).save();
-    console.log(user);
-    return res.status(201).json({
-      status: 'success',
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    res.status(401).json({ status: 'failure', error: { error } });
-  }
-};
+  const user = await new UserModel({
+    name,
+    userName,
+    sector,
+    role,
+    password,
+  }).save();
+  console.log(user);
+  return res.status(201).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
 
 module.exports = { getAllUsers, createUser, getUserById };
