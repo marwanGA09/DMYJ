bca2a9c275fefd4bd837b5b4b1ba1fd83b90ae49
Adem Kedir <ademkedir724@gmail.com>
2024-08-15 17:43:01 +0300

    create apiFeature class to make reusable code

--- server/controllers/membersController.js
+++ server/controllers/membersController.js
@@ -1,32 +1,14 @@
+const APIFeatures = require('../utils/ApiFeatures');
 const catchAsync = require('../utils/catchAsync');
 const MemberModel = require('./../models/memberModel');
 
 const getAllMembers = catchAsync(async (req, res, next) => {
-  const queryObj = { ...req.query };
-  const excludeStr = ['limit', 'sort', 'field', 'page'];
-  excludeStr.forEach((el) => delete queryObj[el]);
-  // FILTERING
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (op) => `$${op}`);
-  let query = MemberModel.find(JSON.parse(queryStr));
+  const features = new APIFeatures(MemberModel.find(), req.query)
+    .filter()
+    .sort()
+    .field();
 
-  // SORTING
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort('name -signedDate');
-  }
-
-  // fields
-  if (req.query.field) {
-    const select = req.query.field.split(',').join(' ');
-    query = query.select(`${select}`);
-  } else {
-    query = query.select('-__v -updatedAt');
-  }
-
-  const members = await query;
+  const members = await features.query;
   return res.status(200).json({
     status: 'success',
     totalResult: members.length,
---
+++ server/utils/ApiFeatures.js
@@ -0,0 +1,39 @@
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludeStr = ['limit', 'sort', 'field', 'page'];
+    excludeStr.forEach((el) => delete queryObj[el]);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (op) => `$${op}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('name -signedDate');
+    }
+
+    return this;
+  }
+
+  field() {
+    if (this.queryString.field) {
+      const select = this.queryString.field.split(',').join(' ');
+      this.query = this.query.select(`${select}`);
+    } else {
+      this.query = this.query.select('-__v -updatedAt');
+    }
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
